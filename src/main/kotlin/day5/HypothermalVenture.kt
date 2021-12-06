59668d31dd64ef9da6e64f20a4166703ef351e76
Carst Tankink <ctankink@bol.com>
2021-12-06 09:30:21 +0100

    AoC-2021 Day 5: Diagonal covers.
    This solution is rather slow because I'm generating all cover points, instead of testing bounds

--- src/main/kotlin/day5/HypothermalVenture.kt
+++ src/main/kotlin/day5/HypothermalVenture.kt
@@ -7,13 +7,14 @@ data class Line(val start: Point, val end: Point) {
     fun isHorizontal(): Boolean = start.y == end.y
     fun isVertical(): Boolean = start.x == end.x
 
+    private val xCover: Set<Long> = (if (start.x <= end.x) (start.x..end.x) else (start.x downTo end.x)).toSet()
+    private val yCover: Set<Long> = (if (start.y <= end.y) (start.y..end.y) else (start.y downTo end.y)).toSet()
+
     fun cover(): Set<Point> = when {
-        isHorizontal() && start.x < end.x -> (start.x..end.x).map { Point(it, start.y) }.toSet()
-        isHorizontal() && start.x > end.x -> (start.x downTo end.x).map { Point(it, start.y) }.toSet()
-        isVertical() && start.y < end.y -> (start.y..end.y).map { Point(start.x, it) }.toSet()
-        isVertical() && start.y > end.y -> (start.y downTo end.y).map { Point(start.x, it) }.toSet()
-        else -> error("Not implement for diagonals")
-    }
+        isHorizontal() -> xCover.map { Point(it, start.y) }
+        isVertical() -> yCover.map { Point(start.x, it) }
+        else -> xCover.zip(yCover).map { Point(it.first, it.second) }
+    }.toSet()
 
     fun overlapping(next: Line): Set<Point> = next.cover().intersect(this.cover())
 }
@@ -29,23 +30,22 @@ class HypothermalVenture(fileName: String) : Solution<Line, Long>(fileName) {
         return Line(points[0], points[1])
     }
 
-    override fun List<Line>.solve1(): Long {
-        tailrec fun overlap(points: Set<Point>, remaining: List<Line>): Set<Point> {
-            return if (remaining.isEmpty()) points else {
-                val head = remaining[0]
-                val tail = remaining.drop(1)
-                val newPoints = tail.flatMap { it.overlapping(head) }.toSet()
-                overlap(points + newPoints, tail)
-            }
+    tailrec fun overlap(points: Set<Point>, remaining: List<Line>): Set<Point> {
+        return if (remaining.isEmpty()) points else {
+            val head = remaining[0]
+            val tail = remaining.drop(1)
+            val newPoints = tail.flatMap { it.overlapping(head) }.toSet()
+            overlap(points + newPoints, tail)
         }
+    }
 
+    override fun List<Line>.solve1(): Long {
         val straightLines = this.filter { it.isVertical() || it.isHorizontal() }
-
         return overlap(emptySet(), straightLines).count().toLong()
     }
 
     override fun List<Line>.solve2(): Long {
-        TODO("Not yet implemented")
+        return overlap(emptySet(), this).count().toLong()
     }
 
 }

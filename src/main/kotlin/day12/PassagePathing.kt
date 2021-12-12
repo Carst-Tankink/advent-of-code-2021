83a7e4c571faee7da083f15490a2a8cd9a5624e1
Carst Tankink <ctankink@bol.com>
2021-12-12 19:42:58 +0100

    AoC-2021 Day 11: Refactor for star 2

--- src/main/kotlin/day12/PassagePathing.kt
+++ src/main/kotlin/day12/PassagePathing.kt
@@ -9,17 +9,31 @@ class PassagePathing(fileName: String) : Solution<Pair<String, String>, Long>(fi
     }
 
     override fun List<Pair<String, String>>.solve1(): Long {
-        val extraEdges = this.map { Pair(it.second, it.first) }
-        val graph: Map<String, List<String>> = (this + extraEdges).groupBy({ it.first }) { it.second }
+        val paths = explorePaths()
+        return paths.size.toLong()
+    }
 
-        fun findPaths(node: String, visited: Set<String>): List<List<String>> {
-            return if (node == "end") listOf(listOf(node)) else
-                graph[node]?.filter { it.all { c -> c.isUpperCase() || it !in visited } }
-                    ?.flatMap { findPaths(it, visited + node).map { p -> listOf(node) + p } }
-                    ?: emptyList()
+    private fun List<Pair<String, String>>.explorePaths(allowedSmall: String? = null): Set<List<String>> {
+        val returnEdges = this.map { Pair(it.second, it.first) }
+        val graph: Map<String, List<String>> = (this + returnEdges).groupBy({ it.first }) { it.second }
+
+        fun findPaths(node: String, visited: Set<String>, allowedSmall: String?): Set<List<String>> {
+            return if (node == "end") setOf(listOf(node)) else {
+                val newVisited = if (node.all { it.isUpperCase() } || node == allowedSmall) visited else visited + node
+                graph[node]?.filter { it !in visited }
+                    ?.flatMap {
+                        findPaths(
+                            it,
+                            newVisited,
+                            if (node == allowedSmall) null else allowedSmall
+                        ).map { p -> listOf(node) + p }
+                    }
+                    ?.toSet()
+                    ?: emptySet()
+            }
         }
 
-        return findPaths("start", setOf()).size.toLong()
+        return findPaths("start", setOf(), allowedSmall)
     }
 
     override fun List<Pair<String, String>>.solve2(): Long {

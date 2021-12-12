c046d26f1a49d81b0b7165412028a51fb8a7909d
Carst Tankink <ctankink@bol.com>
2021-12-12 15:16:56 +0100

    AoC-2021 Day 11: Star 2

--- src/main/kotlin/day11/DumboOctopus.kt
+++ src/main/kotlin/day11/DumboOctopus.kt
@@ -11,23 +11,6 @@ class DumboOctopus(fileName: String) : Solution<List<Int>, Long>(fileName) {
 
     override fun List<List<Int>>.solve1(): Long {
         val grid = this.toGrid()
-        tailrec fun flash(g: Grid<Int>, flashed: Set<Point>): Pair<Set<Point>, Grid<Int>> {
-            val toFlash = g
-                .filterValues { it > 9 }
-                .filterKeys { it !in flashed }
-
-            return if (toFlash.isEmpty()) Pair(flashed, g) else {
-                val nextFlash = toFlash.keys.first()
-                val neighbours = nextFlash.getNeighbours(cardinal = false)
-                flash(g.mapValues { if (it.key in neighbours) it.value + 1 else it.value }, flashed + nextFlash)
-            }
-        }
-
-        fun runStep(g: Grid<Int>): Pair<Long, Grid<Int>> {
-            val increased = g.mapValues { it.value + 1 }
-            val (flashed, newGrid) = flash(increased, emptySet())
-            return Pair(flashed.size.toLong(), newGrid.mapValues { if (it.value > 9) 0 else it.value })
-        }
 
         tailrec fun runSteps(currentStep: Int, flashed: Long, g: Grid<Int>): Long {
             return if (currentStep == 100) flashed else {
@@ -40,6 +23,33 @@ class DumboOctopus(fileName: String) : Solution<List<Int>, Long>(fileName) {
     }
 
     override fun List<List<Int>>.solve2(): Long {
-        TODO("Not yet implemented")
+        val grid = this.toGrid()
+
+        tailrec fun runSteps(currentStep: Long, flashed: Long, g: Grid<Int>): Long {
+            return if (g.all { it.value == 0 }) currentStep else {
+                val (flashStep: Long, newGrid: Grid<Int>) = runStep(g)
+                runSteps(currentStep + 1, flashed + flashStep, newGrid)
+            }
+        }
+
+        return runSteps(0, 0, grid)
+    }
+
+    private tailrec fun flash(g: Grid<Int>, flashed: Set<Point>): Pair<Set<Point>, Grid<Int>> {
+        val toFlash = g
+            .filterValues { it > 9 }
+            .filterKeys { it !in flashed }
+
+        return if (toFlash.isEmpty()) Pair(flashed, g) else {
+            val nextFlash = toFlash.keys.first()
+            val neighbours = nextFlash.getNeighbours(cardinal = false)
+            flash(g.mapValues { if (it.key in neighbours) it.value + 1 else it.value }, flashed + nextFlash)
+        }
+    }
+
+    private fun runStep(g: Grid<Int>): Pair<Long, Grid<Int>> {
+        val increased = g.mapValues { it.value + 1 }
+        val (flashed, newGrid) = flash(increased, emptySet())
+        return Pair(flashed.size.toLong(), newGrid.mapValues { if (it.value > 9) 0 else it.value })
     }
 }

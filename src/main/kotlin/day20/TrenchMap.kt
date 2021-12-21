dc25cad85fe45aef7f84d15c3e2c736ed5da88b4
Carst Tankink <ctankink@bol.com>
2021-12-21 20:06:40 +0100

    AoC-2021 Day 20 Star 2

--- src/main/kotlin/day20/TrenchMap.kt
+++ src/main/kotlin/day20/TrenchMap.kt
@@ -2,7 +2,6 @@ package day20
 
 import util.Grid
 import util.Helpers
-import util.Helpers.Companion.printGrid
 import util.Helpers.Companion.toGrid
 import util.Point
 import util.Solution
@@ -12,32 +11,42 @@ class TrenchMap(fileName: String) : Solution<List<Boolean>, Long>(fileName) {
         return if (line.isEmpty()) null else line.map { it == '#' }
     }
 
-    override fun List<List<Boolean>>.solve1(): Long {
-        val algorithm = this[0]
-        fun enhance(grid: Grid<Boolean>, second: Boolean): Grid<Boolean> {
-            val minX = grid.minOf { it.key.x }
-            val maxX = grid.maxOf { it.key.x }
-            val minY = grid.minOf { it.key.y }
-            val maxY = grid.maxOf { it.key.y }
-            val points = (minY - 1..maxY + 1).flatMap { y ->
-                (minX - 1..maxX + 1).map { x -> Point(x, y) }
-            }
-
-            return points.associateWith {
-                val bits = it.getAllNeighbours()
-                    .map { n -> grid[n] ?: second }
-                    .map { b -> if (b) 1 else 0 }
-                algorithm[Helpers.toDecimal(bits, 2).toInt()]
-            }
+    private fun enhance(grid: Grid<Boolean>, second: Boolean, algorithm: List<Boolean>): Grid<Boolean> {
+        val minX = grid.minOf { it.key.x }
+        val maxX = grid.maxOf { it.key.x }
+        val minY = grid.minOf { it.key.y }
+        val maxY = grid.maxOf { it.key.y }
+        val points = (minY - 1..maxY + 1).flatMap { y ->
+            (minX - 1..maxX + 1).map { x -> Point(x, y) }
         }
 
-        val enhanced = enhance(this.drop(1).toGrid(), false)
-        val enhancedTwice = enhance(enhanced, algorithm[0])
+        return points.associateWith {
+            val bits = it.getAllNeighbours()
+                .map { n -> grid[n] ?: second }
+                .map { b -> if (b) 1 else 0 }
+            algorithm[Helpers.toDecimal(bits, 2).toInt()]
+        }
+    }
+
+    override fun List<List<Boolean>>.solve1(): Long {
+        val algorithm = this[0]
+
+        val enhanced = enhance(this.drop(1).toGrid(), false, algorithm)
+        val enhancedTwice = enhance(enhanced, algorithm[0], algorithm)
         return enhancedTwice.filter { it.value }.size.toLong()
     }
 
 
     override fun List<List<Boolean>>.solve2(): Long {
-        TODO("Not yet implemented")
+        val algorithm = this[0]
+        tailrec fun applySteps(step: Int, grid: Grid<Boolean>): Grid<Boolean> {
+            return if (step == 25) grid else {
+                val enhanced = enhance(grid, false, algorithm)
+                val enhancedTwice = enhance(enhanced, algorithm[0], algorithm)
+                applySteps(step + 1, enhancedTwice)
+            }
+        }
+
+        return applySteps(0, this.drop(1).toGrid()).filter { it.value }.size.toLong()
     }
 }
